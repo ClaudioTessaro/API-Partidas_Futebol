86a2ddcd997ae42e95a14c52f7afc26e74134f99
alinewornath <aline.wornath@gmail.com>
2025-06-12 12:26:37 -0300

    Adiciona logs

--- src/main/java/com/NeoCamp/Desafio_Futebol/exception/GlobalExceptionHandler.java
+++ src/main/java/com/NeoCamp/Desafio_Futebol/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.NeoCamp.Desafio_Futebol.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,14 +9,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<String> handleNaoEncontradoException(EntidadeNaoEncontradaException e) {
+        log.error("Entidade não encontrada: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> handleNegocioException(NegocioException e) {
+        log.error("NegocioException ao processar requisição: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOutrosErros(Exception e) {
+        log.error("Erro inesperado: ", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro inesperado");
     }
 }
