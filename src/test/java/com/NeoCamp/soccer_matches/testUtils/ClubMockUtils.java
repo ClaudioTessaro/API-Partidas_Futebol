da130a81ea40e88f19e2d9cb16de9a51e39c079c
alinewornath <aline.wornath@gmail.com>
2025-06-23 19:09:51 -0300

    Adiciona ClubMockUtils e StateMockUtils

---
+++ src/test/java/com/NeoCamp/soccer_matches/testUtils/ClubMockUtils.java
@@ -0,0 +1,66 @@
+package com.neocamp.soccer_matches.testUtils;
+
+import com.neocamp.soccer_matches.dto.ClubRequestDto;
+import com.neocamp.soccer_matches.dto.ClubResponseDto;
+import com.neocamp.soccer_matches.dto.StateResponseDto;
+import com.neocamp.soccer_matches.entity.ClubEntity;
+import com.neocamp.soccer_matches.entity.StateEntity;
+
+import java.time.LocalDate;
+
+public class ClubMockUtils {
+    public static ClubEntity gremio() {
+        return new ClubEntity("Grêmio", StateMockUtils.rs(),
+                LocalDate.of(1920, 5, 30), true);
+    }
+
+    public static ClubEntity flamengo() {
+        return new ClubEntity("Flamengo", StateMockUtils.rj(),
+                LocalDate.of(1950, 7,15), true);
+    }
+
+    public static ClubEntity corinthians() {
+        return new ClubEntity("Corinthians", StateMockUtils.sp(),
+                LocalDate.of(1971, 6, 23), true);
+    }
+
+    public static ClubEntity inativoSp() {
+        return new ClubEntity("inativo", StateMockUtils.sp(),
+                LocalDate.of(2000, 3, 17), false);
+    }
+
+    public static ClubEntity customEntity(String name, StateEntity homeState,
+                                          LocalDate creationDate, Boolean active) {
+        return new ClubEntity(name, homeState, creationDate, active);
+    }
+
+
+    public static ClubResponseDto gremioResponseDto() {
+        return new ClubResponseDto(1L, "Grêmio", StateMockUtils.rsDto(),
+                LocalDate.of(2003, 1, 10), true);
+    }
+
+    public static ClubResponseDto flamengoResponseDto() {
+        return new ClubResponseDto(2L, "Flamengo", StateMockUtils.rjDto(),
+                LocalDate.of(1900, 2, 25), true);
+    }
+
+    public static ClubResponseDto corinthiansResponseDto() {
+        return new ClubResponseDto(3L, "Corinthians", StateMockUtils.spDto(),
+                LocalDate.of(1930, 4, 19), true);
+    }
+
+    public static ClubResponseDto customResponseDto(String name, StateResponseDto homeState,
+                                               LocalDate creationDate, Boolean active) {
+        return new ClubResponseDto(null, name, homeState, creationDate, active);
+    }
+
+
+    public static ClubRequestDto gremioRequestDto() {
+        return new ClubRequestDto("Grêmio", "RS",  LocalDate.of(2005, 9, 15), true);
+    }
+
+    public static ClubRequestDto customRequestDto(String name, String stateCode, LocalDate creationDate, Boolean active) {
+        return new ClubRequestDto(name, stateCode, creationDate, active);
+    }
+}
---
+++ src/test/java/com/NeoCamp/soccer_matches/testUtils/StateMockUtils.java
@@ -0,0 +1,40 @@
+package com.neocamp.soccer_matches.testUtils;
+
+import com.neocamp.soccer_matches.dto.StateResponseDto;
+import com.neocamp.soccer_matches.entity.StateEntity;
+import com.neocamp.soccer_matches.enums.StateCode;
+
+public class StateMockUtils {
+    public static StateEntity rs() {
+        return new StateEntity("Rio Grande do Sul", StateCode.RS);
+    }
+
+    public static StateEntity sp() {
+        return new StateEntity("São Paulo", StateCode.SP);
+    }
+
+    public static StateEntity rj() {
+        return new StateEntity("Rio de Janeiro", StateCode.RJ);
+    }
+
+    public static StateEntity custom(String name, StateCode stateCode) {
+        return new StateEntity(name, stateCode);
+    }
+
+
+    public static StateResponseDto rsDto() {
+        return new StateResponseDto(1L,"Rio Grande do Sul", "RS");
+    }
+
+    public static StateResponseDto spDto() {
+        return new StateResponseDto(2L, "São Paulo", "SP");
+    }
+
+    public static StateResponseDto rjDto() {
+        return new StateResponseDto(3L,"Rio de Janeiro", "RJ");
+    }
+
+    public static StateResponseDto custom(String name, String stateCode) {
+        return new StateResponseDto(null, name, stateCode);
+    }
+}
