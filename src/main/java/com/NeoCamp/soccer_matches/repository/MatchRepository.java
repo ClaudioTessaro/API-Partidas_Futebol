5e64238dde8ef5018816107de06938d813d162a5
alinewornath <aline.wornath@gmail.com>
2025-06-26 14:51:44 -0300

    Ajusta busca de retrospecto contra adversários para retornar lista

--- src/main/java/com/NeoCamp/soccer_matches/repository/MatchRepository.java
+++ src/main/java/com/NeoCamp/soccer_matches/repository/MatchRepository.java
@@ -12,6 +12,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatchRepository extends JpaRepository<MatchEntity, Long> {
     @Query("""
@@ -27,43 +29,42 @@ public interface MatchRepository extends JpaRepository<MatchEntity, Long> {
 
     @Query("""
         SELECT new com.neocamp.soccer_matches.dto.club.ClubStatsResponseDto(
-            :clubId,
-            (SELECT c.name FROM ClubEntity c WHERE c.id = :clubId),
-            COUNT(CASE WHEN (m.homeClub.id = :clubId AND m.homeGoals > m.awayGoals)
-                   OR (m.awayClub.id = :clubId AND m.awayGoals > m.homeGoals) THEN 1 END),
+            :id,
+            (SELECT c.name FROM ClubEntity c WHERE c.id = :id),
+            COUNT(CASE WHEN (m.homeClub.id = :id AND m.homeGoals > m.awayGoals)
+                   OR (m.awayClub.id = :id AND m.awayGoals > m.homeGoals) THEN 1 END),
             COUNT(CASE WHEN m.homeGoals = m.awayGoals THEN 1 END),
-            COUNT(CASE WHEN (m.homeClub.id = :clubId AND m.homeGoals < m.awayGoals)
-                   OR (m.awayClub.id = :clubId AND m.awayGoals < m.homeGoals) THEN 1 END),
-            SUM(CASE WHEN m.homeClub.id = :clubId THEN m.homeGoals
-                     WHEN m.awayClub.id = :clubId THEN m.awayGoals ELSE 0 END),
-            SUM(CASE WHEN m.homeClub.id = :clubId THEN m.awayGoals
-                     WHEN m.awayClub.id = :clubId THEN m.homeGoals ELSE 0 END)
+            COUNT(CASE WHEN (m.homeClub.id = :id AND m.homeGoals < m.awayGoals)
+                   OR (m.awayClub.id = :id AND m.awayGoals < m.homeGoals) THEN 1 END),
+            SUM(CASE WHEN m.homeClub.id = :id THEN m.homeGoals
+                     WHEN m.awayClub.id = :id THEN m.awayGoals ELSE 0 END),
+            SUM(CASE WHEN m.homeClub.id = :id THEN m.awayGoals
+                     WHEN m.awayClub.id = :id THEN m.homeGoals ELSE 0 END)
         )
         FROM MatchEntity m
-        WHERE m.homeClub.id = :clubId OR m.awayClub.id = :clubId
+        WHERE m.homeClub.id = :id OR m.awayClub.id = :id
     """)
-    ClubStatsResponseDto getClubStats(@Param("clubId") Long clubId);
+    ClubStatsResponseDto getClubStats(@Param("id") Long id);
 
     @Query("""
         SELECT new com.neocamp.soccer_matches.dto.club.ClubVersusClubStatsDto(
-            :clubId,
-            (SELECT c.name FROM ClubEntity c WHERE c.id = :clubId),
-            :opponentId,
-            (SELECT c.name FROM ClubEntity c WHERE c.id = :opponentId),
-            COUNT(CASE WHEN (m.homeClub.id = :clubId AND m.awayClub.id = :opponentId AND m.homeGoals > m.awayGoals)
-                OR (m.awayClub.id = :clubId AND m.homeClub.id = :opponentId AND m.awayGoals > m.homeGoals)
-                 THEN 1 END),
+            :id,
+            (SELECT c.name FROM ClubEntity c WHERE c.id = :id),
+            CASE WHEN m.homeClub.id = :id THEN m.awayClub.id ELSE m.homeClub.id END,
+            CASE WHEN m.homeClub.id = :id THEN m.awayClub.name ELSE m.homeClub.name END,
+            COUNT(CASE WHEN (m.homeClub.id = :id AND m.homeGoals > m.awayGoals)
+                OR (m.awayClub.id = :id AND m.awayGoals > m.homeGoals) THEN 1 END),
             COUNT(CASE WHEN m.homeGoals = m.awayGoals THEN 1 END),
-            COUNT(CASE WHEN (m.homeClub.id = :clubId AND m.homeGoals < m.awayGoals)
-                OR (m.awayClub.id = :clubId AND m.awayGoals < m.homeGoals) THEN 1 END),
-            SUM(CASE WHEN m.homeClub.id = :clubId THEN m.homeGoals
-                     WHEN m.awayClub.id = :clubId THEN m.awayGoals ELSE 0 END),
-            SUM(CASE WHEN m.homeClub.id = :clubId THEN m.awayGoals
-                     WHEN m.awayClub.id = :clubId THEN m.homeGoals ELSE 0 END)
+            COUNT(CASE WHEN (m.homeClub.id = :id AND m.homeGoals < m.awayGoals)
+                OR (m.awayClub.id = :id AND m.awayGoals < m.homeGoals) THEN 1 END),
+            SUM(CASE WHEN m.homeClub.id = :id THEN m.homeGoals ELSE m.awayGoals END),
+            SUM(CASE WHEN m.homeClub.id = :id THEN m.awayGoals ELSE m.homeGoals END)
        )
        FROM MatchEntity m
-       WHERE (m.homeClub.id = :clubId AND m.awayClub.id = :opponentId)
-            OR (m.awayClub.id = :clubId AND m.homeClub.id = :opponentId)
+       WHERE (m.homeClub.id = :id OR m.awayClub.id = :id)
+       GROUP BY
+            CASE WHEN m.homeClub.id = :id THEN m.awayClub.id ELSE m.homeClub.id END,
+            CASE WHEN m.homeClub.id = :id THEN m.awayClub.name ELSE m.homeClub.name END
 """)
-    ClubVersusClubStatsDto getClubVersusClubStats(@Param("clubId") Long clubId, @Param("opponentId") Long opponentId);
+    List<ClubVersusClubStatsDto> getClubVersusOpponentsStats(@Param("id") Long id);
 }
